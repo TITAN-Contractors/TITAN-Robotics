11c239b6f0346e5eb707ca1f2aa65f6fd8d3fda2
SDTheCyanWyan <shouryabisw@gmail.com>
2020-12-07 21:23:31 +0530

    Adds Live Member Count

--- index.js
+++ index.js
@@ -9,16 +9,32 @@ const client = new Discord.Client();
 // this event will only trigger one time after logging in
 client.once('ready', () => {
 	console.log('Ready!');
+	setInterval(() => {
+		targetGuild = client.guilds.cache.get('607930707138052108')
+		if (targetGuild) {
+			client.user.setPresence({
+				activity: {
+					name: targetGuild.memberCount + ' contractors blaze the trail!',
+					type: 'WATCHING'
+				},
+				status: 'online'
+			})
+				.then(console.log)
+				.catch(console.error);
+		}
+	}, 1000 * 60 * 5);
+
 });
 
 client.on('message', message => {
-	if (message.content.startsWith === `${prefix}ping`) {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
 		// send back "Pong." to the channel the message was sent in
 		message.channel.send('Pong.');
-	}
-	else if (message.content === `${prefix}contractors`) {
-		// show member numbers
-		message.channel.send(`There are currently ${message.guild.memberCount} contractors!`)
 	}
 });
 

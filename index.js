2a6d187e34baa56de9e6d9bbd6b23defe464d1e7
SDTheCyanWyan <shouryabisw@gmail.com>
2020-12-11 19:24:43 +0530

    Add Command Handlers

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-// require the discord.js module
+// require the modules
+const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
 
 // create a new Discord client
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
@@ -30,11 +38,17 @@ client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
+	const commandName = args.shift().toLowerCase();
 
-	if (command === 'ping') {
-		// send back "Pong." to the channel the message was sent in
-		message.channel.send('Pong.');
+	if (!client.commands.has(commandName)) return;
+
+	const command = client.commands.get(commandName);
+
+	try {
+		command.execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
 	}
 });
 
